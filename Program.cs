7aa645ad95b725a19e76bb8dffeb754349b888cb
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-11 10:43:05 -0400

    Message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Print(int x)
+        {
+            Console.WriteLine(x/2);
+        }
+
+        static void Main(string[] args)
+        {
+            Print(42);
+            Print(50);
+            Print(100);
+        }
+
+    }
+}
+
